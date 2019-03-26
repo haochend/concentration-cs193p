449c4d121b1611529498b0626ab47aa19595599f
haochend <dong.haocheng95@gmail.com>
2019-03-26 12:23:00 -0700

    try GitHub

--- concentration/ViewController.swift
+++ concentration/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    // trying out github
     
     var numberOfPairsOfCards: Int {
         return (cardButtons.count+1)/2
